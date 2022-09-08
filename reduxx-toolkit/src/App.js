aed3b1badd44ea12d3c57dd5dd2aa9f5afbfe83e
yogjin <youngjin988@gmail.com>
2022-09-08 14:10:13 +0900

    redux-toolkit init

---
+++ reduxx-toolkit/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function Counter() {
+  return (
+    <>
+      <h1>0</h1>
+      <button>+</button>
+    </>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
