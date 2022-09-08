db0c500d1ef52fd8ff0bdf4d53c31bc603a608cf
yogjin <youngjin988@gmail.com>
2022-09-08 14:41:02 +0900

    1. redux, react-redux만 가지고 Counter 컴포넌트 생성

--- reduxx-toolkit/src/App.js
+++ reduxx-toolkit/src/App.js
@@ -1,18 +1,34 @@
-import './App.css';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+
+function reducer(state, action) {
+  if (action.type === 'up') {
+    return { ...state, number: state.number + action.step };
+  }
+  const newState = { ...state };
+  return newState;
+}
+const initialState = { number: 0 };
+const store = createStore(reducer, initialState);
 
 function Counter() {
+  const count = useSelector((state) => state.number);
+  const dispatch = useDispatch();
   return (
     <>
-      <h1>0</h1>
-      <button>+</button>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch({ type: 'up', step: 2 })}>+</button>
     </>
   );
 }
 function App() {
   return (
-    <div>
-      <Counter />
-    </div>
+    <Provider store={store}>
+      <div>
+        <Counter />
+      </div>
+    </Provider>
   );
 }
 
