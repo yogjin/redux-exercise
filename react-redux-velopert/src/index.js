77cafedcadc7541a48a34459257beca4ccfa1030
yogjin <youngjin988@gmail.com>
2022-09-14 00:37:46 +0900

    velopert-redux: redux toolkit의 configureStore 이용해 store 생성

--- react-redux-velopert/src/index.js
+++ react-redux-velopert/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './modules';
+
+const store = configureStore({ reducer: rootReducer });
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
